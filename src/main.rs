dd6de7907da3c489b8a5bb141d42b0b856782965
Mattingly, Bret <Bret.Mattingly@nextgearcapital.com>
2017-03-14 15:27:15 -0400

    code reorg and terminal controller creation

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,18 @@
 extern crate termion;
 
 mod constants;
+mod term_control;
 
-use termion::{color, style, clear, terminal_size, cursor};
-use termion::raw::{RawTerminal,IntoRawMode};
-use termion::event::Key;
-use std::io::{Write, Stdout, stdout};
-use constants::{BOX_CHARS_BLD, BOX_CHARS_STD};
+use term_control::TermControl;
+use termion::clear;
+
+
 
 fn main() {
 
-    init();
     println!("{}", clear::All);
-
-}
-
-fn init() {
-    border_draw();
-}
-
-fn border_draw() {
+    let mut tc = TermControl::new();
+    tc.run();
     println!("{}", clear::All);
-
-    let mut stdout = stdout().into_raw_mode().unwrap();
-    let (width, height) = terminal_size().unwrap();
-
-    // Corners
-    // Stupid Goto is 1-indexed
-    write!(stdout, "{}{}", cursor::Goto(1,1), BOX_CHARS_BLD[2]);
-    write!(stdout, "{}{}", cursor::Goto(width+1,1), BOX_CHARS_BLD[3]);
-    write!(stdout, "{}{}", cursor::Goto(1,height+1), BOX_CHARS_BLD[4]);
-    write!(stdout, "{}{}", cursor::Goto(width+1,height+1), BOX_CHARS_BLD[5]);
-
-    for x in 2..width {
-        write!(stdout, "{}{}", cursor::Goto(x,1), BOX_CHARS_BLD[0]);
-        write!(stdout, "{}{}", cursor::Goto(x,height+1), BOX_CHARS_BLD[0]);
-    }
-
-    for y in 2..height {
-        write!(stdout, "{}{}", cursor::Goto(1,y), BOX_CHARS_BLD[1]);
-        write!(stdout, "{}{}", cursor::Goto(width+1,y), BOX_CHARS_BLD[1]);
-    }
 
 }
---
+++ src/term_control.rs
@@ -0,0 +1,72 @@
+use constants::{BOX_CHARS_BLD, BOX_CHARS_STD};
+
+use termion::{async_stdin, color, style, clear, terminal_size, cursor, AsyncReader};
+use termion::raw::{RawTerminal,IntoRawMode};
+use termion::event::Key;
+use termion::input::TermRead;
+
+use std::io::{Read, Write, stdout, Stdout, stdin};
+use std::thread::sleep;
+use std::time::Duration;
+
+pub struct TermControl {
+    stdin: AsyncReader,
+    stdout: RawTerminal<Stdout>,
+    read_buf: [u8;1024],
+    write_buf: [u8;1024],
+    key_buf: [u8;4],
+    width: u16,
+    height: u16
+}
+// Should ultimately put "state" in a struct to make borrowing, etc, of stdin and like easier.
+
+impl TermControl {
+    pub fn new() -> TermControl {
+        let (width, height) = terminal_size().unwrap();
+        TermControl {
+            stdin: async_stdin(),
+            stdout: stdout().into_raw_mode().unwrap(),
+            read_buf: [0; 1024],
+            write_buf: [0; 1024],
+            key_buf: [0;4],
+            width: width,
+            height: height
+        }
+    }
+
+    fn border_draw(&mut self) {
+        println!("{}", clear::All);
+        write!(self.stdout, "{}{}", cursor::Goto(1,1), BOX_CHARS_BLD[2]);
+        write!(self.stdout, "{}{}", cursor::Goto(self.width+1,1), BOX_CHARS_BLD[3]);
+        write!(self.stdout, "{}{}", cursor::Goto(1,self.height+1), BOX_CHARS_BLD[4]);
+        write!(self.stdout, "{}{}", cursor::Goto(self.width+1,self.height+1), BOX_CHARS_BLD[5]);
+
+        for x in 2..self.width {
+            write!(self.stdout, "{}{}", cursor::Goto(x,1), BOX_CHARS_BLD[0]);
+            write!(self.stdout, "{}{}", cursor::Goto(x,self.height+1), BOX_CHARS_BLD[0]);
+        }
+
+        for y in 2..self.height {
+            write!(self.stdout, "{}{}", cursor::Goto(1,y), BOX_CHARS_BLD[1]);
+            write!(self.stdout, "{}{}", cursor::Goto(self.width+1,y), BOX_CHARS_BLD[1]);
+        }
+        self.stdout.flush().unwrap();
+    }
+
+    fn input_capture(&mut self) {
+        let fuck = self.stdin.read(&mut self.key_buf);
+
+        match fuck {
+            Ok(n)   =>  writeln!(self.stdout, "{}{:?}", cursor::Goto(2,2), &self.key_buf).unwrap(),
+            Err(e)  =>  writeln!(self.stdout, "{}Error : {:?}", cursor::Goto(10,10), e).unwrap()
+        }
+    }
+
+    pub fn run(&mut self) {
+        self.border_draw();
+        loop {
+            self.input_capture();
+            sleep(Duration::from_millis(1000));
+        }
+    }
+}
